0abb47735b4487859dd6bfc8676db7e565cb86f4
Kneba <abenkenary3@gmail.com>
2022-12-07 21:34:14 +0700

    Superior [ci skip]

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kneba/local_manifests --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b twelvedotone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Kneba/local_manifests --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch superior_X00TD-userdebug
 export TZ=Asia/Jakarta
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-mka bacon -j8
+m bacon -j8
 # end
