4d3211587c73f3af1869c745280a5168df021e98
Kneba <abenkenary3@gmail.com>
2022-12-09 05:16:43 +0700

    atest again

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b twelvedotone -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kneba/local_manifests --depth 1 -b superior .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Kneba/local_manifests --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch superior_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta
-#export ALLOW_MISSING_DEPENDENCIES=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 m bacon -j8
 # end
