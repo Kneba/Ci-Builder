b9165df4a410ba408e8f1ef741dd792a540dc595
Kneba <abenkenary3@gmail.com>
2022-12-11 06:41:59 +0700

    try

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Kneba/android -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kneba/local_manifests --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export ALLOW_MISSING_DEPENDENCIES=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 m bacon -j8
 # end
